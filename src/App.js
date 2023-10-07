6ee369c2fe16daa542cd3a5e309a72269634314c
cubi-dev <cubiiwork@gmail.com>
2023-10-07 10:29:14 +0700

    Finished file structure setup Add Routes that we need from here

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
-import { Navbar, Footer, ThemeSettings } from "./components";
+import { Navbar, Footer, ThemeSettings, Sidebar } from "./components";
 import {
   Onlyfood,
   Orders,
@@ -18,7 +18,7 @@ import {
   Financial,
   ColorPicker,
   ColorMapping,
-  Editor
+  Editor,
 } from "./pages";
 import "./App.css";
 
@@ -48,50 +48,50 @@ const App = () => {
                  dark:bg-secondary-dark-bg
                  bg-white"
             >
-              Sidebar
+              <Sidebar />
             </div>
           ) : (
             <div
               className="w-0
                 dark:bg-secondary-dark-bg"
             >
-              Sidebar w-0
+              <Sidebar />
             </div>
           )}
-          {/* Navbar */}
+          <Navbar />
           <div
             className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
               activeMenu ? "md:ml-72" : "flex-2"
             }`}
           >
             <div className=" fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
-              Navbar
+              <Navbar />
             </div>
           </div>
 
           {/* Routing */}
           <Routes>
             {/* Dashboard */}
-            <Route path="/" element="Onlyfood" />
-            <Route path="/onlyfood" element="Onlyfood" />
+            <Route path="/" element={<Onlyfood />} />
+            <Route path="/onlyfood" element={<Onlyfood />} />
             {/* Pages */}
-            <Route path="/orders" element="Pages" />
-            <Route path="/employees" element="Employees" />
-            <Route path="/customers" element="Customers" />
+            <Route path="/orders" element={<Pages />} />
+            <Route path="/employees" element={<Employees />} />
+            <Route path="/customers" element={<Customers />} />
             {/* Apps */}
-            <Route path="/kanban" element="Kanban" />
-            <Route path="/editor" element="Editor" />
-            <Route path="/calendar" element="Calendar" />
-            <Route path="/color-picker" element="ColorPicker" />
+            <Route path="/kanban" element={<Kanban />} />
+            <Route path="/editor" element={<Editor />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/color-picker" element={<ColorPicker />} />
             {/* Chart */}
-            <Route path="/line" element="Line" />
-            <Route path="/area" element="Area" />
-            <Route path="/bar" element="Bar" />
-            <Route path="/pie" element="Pie" />
-            <Route path="/financial" element="Financial" />
-            <Route path="/color-mapping" element="ColorMapping" />
-            <Route path="/pyramid" element="Pyramid" />
-            <Route path="/stacked" element="Stacked" />
+            <Route path="/line" element={<Line />} />
+            <Route path="/area" element={<Area />} />
+            <Route path="/bar" element={<Bar />} />
+            <Route path="/pie" element={<Pie />} />
+            <Route path="/financial" element={<Financial />} />
+            <Route path="/color-mapping" element={<ColorMapping />} />
+            <Route path="/pyramid" element={<Pyramid />} />
+            <Route path="/stacked" element={<Stacked />} />
           </Routes>
         </div>
       </BrowserRouter>
