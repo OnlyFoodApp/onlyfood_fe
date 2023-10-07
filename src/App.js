4a7ae87c9726357b9bd1f0c420c5f7f50f6cb519
cubi-dev <cubiiwork@gmail.com>
2023-10-07 11:08:56 +0700

    Add icon- heading for sidebar "Onlyfood"

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import {
 import "./App.css";
 
 const App = () => {
-  const activeMenu = false;
+  const activeMenu = true;
   return (
     <div>
       <BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import { MdOutLineCancel, MdFastfood } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { links } from "../data/dummy";
 const Sidebar = () => {
+  const activeMenu = true;
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div
+      className="ml-3 h-screen
+    md:overflow-hidden overflow-auto
+    md:hover:overflow-auto pb-10"
+    >
+      {activeMenu && (
+        <>
+          <div className="flex justify-between items-center">
+            <Link
+              to="/"
+              onClick={() => {}}
+              className=" items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
+            >
+              <MdFastfood />
+              <span>OnlyFood</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
