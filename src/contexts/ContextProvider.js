6176c9b35bed07272e8ba62d57e5476890d6626e
cubi-dev <cubiiwork@gmail.com>
2023-10-09 05:13:32 +0700

    Update navbar include state managerment

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,12 +12,18 @@ const initalState = {
 //Context Provider là để lấy state trong tất cả các component
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initalState);
+  const [screenSize, setScreenSize] = useState(undefined); 
+  const handleClick = (clicked) => {
+    //chi thay doi cac thu da duoc click
+    setIsClicked({...initalState, [clicked]:true})
+  }
   return (
     // Với các value mà chúng ta pass qua cái hook này thì
     // sẽ được pass qua tất cả các component khác
     // Và với các value đó bên dưới chúng sẽ có các children
     // VD: áp dụng cho SideBar open hay close
-    <StateContext.Provider value={{ activeMenu: activeMenu, setActiveMenu }}>
+    <StateContext.Provider value={{activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }}>
       {children}
     </StateContext.Provider>
   );    
