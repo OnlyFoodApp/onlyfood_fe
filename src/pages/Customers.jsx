c111f1b47ea13ed304f78f328b38bf7f392bc647
Trương Đinh Đăng Khoa <khoatdd0811@gmail.com>
2023-10-20 11:02:45 +0700

    call api customer

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -3,25 +3,26 @@ import { ColumnDirective, ColumnsDirective, Filter, GridComponent } from '@syncf
 import { Group, Inject, Page, Sort } from '@syncfusion/ej2-react-grids';
 import axios from 'axios'; // Import Axios
 import { data } from '../data/datasource';
+import callApi from "../utils/APICaller";
 const Customers = () => {
 //call api get data về
 
 
-const [customers, setCustomers] = useState([]); // State để lưu dữ liệu từ API
-
+ const [customers, setCustomers] = useState([]); // State để lưu dữ liệu từ API
+ const [dataLoaded, setDataLoaded] = useState(false);
   useEffect(async () => {
     // Gọi API và cung cấp token trong tiêu đề
     await axios.get('https://onlyfood.azurewebsites.net/api/v1/customers', {
       headers: new Headers({
-        'Accept': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-        'Access-Control-Request-Method': 'GET, POST, DELETE, PUT, OPTIONS',
-        'Authorization': 'Bearer ' + localStorage.getItem('token')
+        'Authorization': 'Bearer ' + localStorage.getItem('token')  
       }),
     })
       .then(response => {
-        setCustomers(response.data); // Lưu dữ liệu API vào state
+        console.log('danh sách : ' , response.data.data);
+        // Lưu dữ liệu API vào state
+         setCustomers(response.data.data);
+         setDataLoaded(true);
+        console.log('customers :',customers );
       })
       .catch(error => {
         console.error('Lỗi khi lấy dữ liệu từ API:', error);
@@ -30,31 +31,116 @@ const [customers, setCustomers] = useState([]); // State để lưu dữ liệu 
 
 
 
-  /// table data
-  const pageSettings = { pageSize: 6 };
-  const sortSettings = { columns: [{ field: 'EmployeeID', direction: 'Ascending' }] };
-  const filterSettings = { columns: [{ field: 'EmployeeID', operator: 'greaterthan', value: 2 }] };
-        
-  return (
-    <GridComponent
-      dataSource={customers}
-      allowPaging={true}
-      pageSettings={pageSettings}
-      filterSettings={filterSettings}
-      allowSorting={true}
-      sortSettings={sortSettings}
-      allowFiltering={true}
-    >
-      <ColumnsDirective>
-        <ColumnDirective field='OrderID' width='100' textAlign="Right"/>
-        <ColumnDirective field='CustomerID' width='100'/>
-        <ColumnDirective field='EmployeeID' width='100' textAlign="Right"/>
-        <ColumnDirective field='Freight' width='100' format="C2" textAlign="Right"/>
-        <ColumnDirective field='ShipCountry' width='100'/>
-      </ColumnsDirective>
-      <Inject services={[Page, Sort, Filter, Group]}/>
-    </GridComponent>
-  );
+
+  //  useEffect( async  () =>  {
+  //   await callApi("api/v1/customers", "GET", null).then((res) => {
+  //     console.log('Du lieu tu api ' , res.data.data);
+  //     setCustomers(res.data.data);
+  //     // console.log('Customer trong await' , customers);
+  //     setDataLoaded(true);
+  //   });
+  //   // console.log('Customer sau await' , customers);
+  // }, []);
+
+
+
+    return (<div>
+
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr  >
+                    <th scope="col" class="p-4">
+                        <div class="flex items-center">
+                            <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                        </div>
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Username
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                    Email
+                    </th>
+                  
+                    <th scope="col" class="px-6 py-3">
+                    FirstName
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                    LastName
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                    DateOfBirth
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                    Gender
+                    </th>
+                    <th scope="col" class="px-6 py-3">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            
+        </table>
+    </div>
+    
+    {dataLoaded ? (
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <tbody>
+            {customers.map((customer, index) => (
+              <tr key={`customers-${index}`} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover-bg-gray-600">
+                 <td class="w-4 p-4">
+                        <div class="flex items-center">
+                            <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                            <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                        </div>
+                    </td>
+                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {customer.account.username}
+                    </th>
+
+                    <td>
+                      
+                    </td>
+                    <td class="px-6 py-4">
+                    {customer.account.email}
+                    </td>
+                   
+                    <td class="px-6 py-4">
+                    {customer.account.firstName}
+                    </td>
+                    <td class="px-6 py-4">
+                    {customer.account.lastName}
+                    </td>
+                    <td class="px-6 py-4">
+                    {customer.account.dateOfBirth}
+                    </td>
+                    <td class="px-6 py-4">
+                    {customer.account.gender}
+                    </td>
+                    <td class="flex items-center px-6 py-4 space-x-3">
+                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                        <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
+                    </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p>Loading...</p>
+      )}
+
+
+
+    
+      </div>) ;
+
+  
+
+
+
+
+  
 }
 
 export default Customers;
