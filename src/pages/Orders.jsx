200c41ffbb9063147490cd93299d98e44d9d0673
Thu Trang Le <lthutrang17@gmail.com>
2023-10-20 11:07:17 +0700

    prepare to merge api branch

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -21,15 +21,31 @@ import {
   ordersData2,
 } from "../data/dummy";
 import { Header } from "../components";
+import { get } from "https";
 
 const Orders = () => {
+  const [orders, setOrders] = React.useState([]);
+  const getOrders = () => {
+    axios
+      .get("https://onlyfood.azurewebsites.net/api/v1/orders")
+      .then((response) => {
+        setOrders(response.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(() => {
+    getOrders();
+  }, []);
+
   const editing = { allowDeleting: true, allowEditing: true };
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
         id="gridcomp"
-        dataSource={ordersData2}
+        dataSource={orders}
         allowPaging
         allowSorting
         allowExcelExport
