fcf800e1c250fe6fb11d75370b1f2fe48428fa20
cubi-dev <cubiiwork@gmail.com>
2024-03-05 01:48:26 +0700

    Fix UI login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -58,7 +58,7 @@ function Login() {
                   <img className="w-8 h-8 mr-2" src={logo} alt="logo" />
                   Pillsy Admin
                 </a>
-                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                <div className="w-96 bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                   <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                       Sign in to your account
