1204ad173b0b98a031fe5f09af656d13ae8b0a21
cubi-dev <cubiiwork@gmail.com>
2024-03-11 11:38:23 +0700

    Update All Orders,Patients,Pill,User Page, ready to edit record items

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -12,44 +12,54 @@ import {
   PdfExport,
   Edit,
   Inject,
-  Toolbar
+  Toolbar,
 } from "@syncfusion/ej2-react-grids";
 import axios from "axios"; // Import Axios
 import { axiosPrivate } from "../api/axiosInstance";
-import { GET_ALL_PATIENTS } from "../api/apiConstants";
+import { GET_ALL_PATIENTS, DATA_OF_PATIENTS } from "../api/apiConstants";
 import { data } from "../data/datasource";
 import { Header } from "../components";
 import { useStateContext } from "../contexts/ContextProvider";
 import { useNavigate } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
 import { contextMenuItems } from "../data/dummy";
 const Patients = () => {
-  //TEST
-  const toolbarOptions = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
-    const editparams = { params: { popupHeight: '300px' } };
-    const validationRule = { required: true };
-    const customeridRules = { required: true, number: true };
-    const pageSettings = { pageCount: 5 };
-    const format = { type: 'dateTime', format: 'M/d/y hh:mm a' };
-    let gridInstance;
-    let dropDownInstance;
-    const droplist = [
-        { text: 'Top', value: 'Top' },
-        { text: 'Bottom', value: 'Bottom' }
-    ];
   const { isClicked, setIsClicked, handleClick, setIsLoggedIn, isLoggedIn } =
     useStateContext();
   const [patients, setPatients] = useState([]);
   const [dataLoaded, setDataLoaded] = useState(false);
   const isMounted = useRef(false); // Add this line
   const navigate = useNavigate();
+  //TEST
+  const toolbarOptions = ["Add", "Edit", "Update","Delete", "Cancel"];
+  const editparams = { params: { popupHeight: "300px" } };
+  const validationRule = { required: true };
+  const customeridRules = { required: true, number: true };
+  const pageSettings = { pageCount: 5 };
+  const format = { type: "dateTime", format: "M/d/y hh:mm a" };
+  let gridInstance;
+  let dropDownInstance;
+  const droplist = [
+    { text: "Top", value: "Top" },
+    { text: "Bottom", value: "Bottom" },
+  ];
 
   useEffect(() => {
     if (!isLoggedIn) {
-      toast.error('User must login first');
-      navigate('/login');
+      toast.error("User must login first");
+      navigate("/login");
     }
   }, []); // Empty dependency array means this effect runs once after the initial render
+
+  const modifyPatientData = (data) => {
+    let patientRes = data.map((item, index) => {
+      return {
+        ...item,
+        index: index + 1,
+      };
+    });
+    return patientRes;
+  };
 
   useEffect(() => {
     isMounted.current = true; // Set the ref to true when the component mounts
@@ -57,14 +67,17 @@ const Patients = () => {
     const fetchData = async () => {
       try {
         const response = await axiosPrivate.get(GET_ALL_PATIENTS);
-        if (response.status === 200 && isMounted.current) { // Check if the component is still mounted
+        if (response.status === 200 && isMounted.current) {
+          // Check if the component is still mounted
           console.log("danh sách patients: ", response.data);
-          setPatients(response.data);
+          const patientData = modifyPatientData(response.data);
+          setPatients(patientData);
           setDataLoaded(true);
           console.log("patients :", patients);
         }
       } catch (error) {
         console.error("Lỗi khi lấy dữ liệu từ API:", error);
+        toast.error("Cannot get the data please refresh or log back in!");
       }
     };
 
@@ -74,55 +87,242 @@ const Patients = () => {
       isMounted.current = false; // Set the ref to false when the component unmounts
     };
   }, []);
+  // CUSTOM genderStatus
+  const genderStatus = [0, 1, 2].map((gender, index) => ({
+    id: index,
+    gender,
+  }));
+  const statusTemplate = (field) => {
+    if (field && field.gender !== undefined) {
+      const gender = genderStatus.find((status) => status.id === field.gender);
+      const genderMap = {
+        0: "Male",
+        1: "Female",
+        2: "Others",
+      };
+      console.log("GENDER UI ", gender);
+      return (
+        <div>
+          <span>{gender ? genderMap[gender.gender] : "Unknown"}</span>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <span>Unknown</span>
+        </div>
+      );
+    }
+  };
 
-  const editing = { allowDeleting: true, allowEditing: true, allowAdding: true, mode: 'Dialog' };
+  
+  //HANDLE UPDATE
+  const updatePatientInState = (oldData, newData) => {
+    setPatients((prevPatient) => {
+      return prevPatient.map((patient) =>
+        patient.patientID === oldData.patientID
+          ? { ...oldData, ...newData }
+          : patient
+      );
+    });
+  };
+  //Status helper
+  const getStatusId = (statusName) => {
+    console.log("Status name: ", statusName); // Log the status name
+    console.log("Gender status: ", genderStatus); // Log the order status array
+    const status = genderStatus.find((status) => status.gender === statusName);
+    return status ? status.id : null;
+  };
+  // CRUD
+  const actionBegin = async (args) => {
+    if (args.data) {
+      console.log("DATA: \n", args.data);
+      console.log("ROW DATA: \n", args.rowData);
+    }
+
+    if (args.requestType === "save") {
+      if (args.action === "add") {
+        // Send a POST request to add a new order
+        await axiosPrivate.post(DATA_OF_PATIENTS, args.data);
+      } else if (args.action === "edit") {
+        // Ensure args.data includes a 'command' field and 'status' is an integer
+        const data = {
+          patientID: args.data.patientID,
+          firstName: args.data.firstName,
+          lastName: args.data.lastName,
+          dateOfBirth: args.data.dateOfBirth,
+          gender: args.data.gender,
+          phoneNumber: args.data.phoneNumber,
+          address: args.data.address,
+          // patient: args.data.patient,
+          // orderDetails: args.data.orderDetails,
+          // createdBy: args.data.createdBy,
+          // createdDate: args.data.createdDate,
+          // modifiedBy: args.data.modifiedBy,
+          // lastModifiedDate: args.data.lastModifiedDate,
+        };
+        const genderId =
+          typeof args.data.gender === "number"
+            ? getStatusId(args.data.gender)
+            : args.data.gender;
+        if (genderId !== null) {
+          data.gender = genderId;
+        }
+        console.log("Data: ", data);
+        console.log("gender:", args.data.gender);
+        // Send a PUT request to update an existing order
+
+        try {
+          // Before sending the PUT request, find the order in the state that you're about to update
+          const patientToUpdate = patients.find(
+            (patient) => patient.patientID === args.data.patientID
+          );
+          await axiosPrivate.put(DATA_OF_PATIENTS, data);
+          updatePatientInState(patientToUpdate, data);
+          toast.success("Update Patient successfully!");
+        } catch (error) {
+          if (error.response) {
+            console.error(error.response.data); // Log the server's error message
+            toast.error("Update Patient failed!");
+          } else {
+            console.error(error.message); // Log the error message
+            toast.error("Update Patient failed!");
+          }
+        }
+      }
+      args.cancel = true; // Cancel the grid update because we've already updated the data source
+    } else if (args.requestType === "delete") {
+      console.log(args.data[0].patientID);
+      const data = {
+        id: args.data[0].patientID,
+      };
+      console.log("DELETE Order id: ", data);
+      // Send a DELETE request to delete an order
+      try {
+        await axiosPrivate.delete(
+          `${DATA_OF_PATIENTS}/${args.data[0].patientID}`
+        );
+        setPatients((prevOrders) =>
+          prevOrders.filter(
+            (patient) => patient.patientID !== args.data[0].patientID
+          )
+        );
+        toast.success("Update Patient successfully!");
+      } catch (error) {
+        if (error.response) {
+          console.error(error.response.data); // Log the server's error message
+          toast.error("Update Patient failed!");
+        } else {
+          console.error(error.message); // Log the error message
+          toast.error("Update Patient failed!");
+        }
+      }
+      args.cancel = true; // Cancel the grid update because we've already updated the data source
+    }
+  };
+
+  const editing = {
+    allowDeleting: true,
+    allowEditing: true,
+    allowAdding: true,
+    mode: "Dialog",
+  };
 
   return (
-     //Mẫu style vui lòng search customersData , customersGrid và Git: https://github.com/adrianhajdin/project_syncfusion_dashboard/blob/main/src/pages/Customers.jsx
+    //Mẫu style vui lòng search customersData , customersGrid và Git: https://github.com/adrianhajdin/project_syncfusion_dashboard/blob/main/src/pages/Customers.jsx
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-      <ToastContainer/>
+      <ToastContainer />
       <Header category="Page" title="Patients" />
       {dataLoaded ? (
         //TESTING
         <GridComponent
-        id="gridcomp"
-        dataSource={patients}
-        allowPaging={true}
-        allowSorting
-        allowExcelExport
-        allowPdfExport
-        editSettings={editing}
-        contextMenuItems={contextMenuItems}
-        toolbar={toolbarOptions}
-        pageSettings={pageSettings}
-        width="100%"
-      >
-        <ColumnsDirective>
-          <ColumnDirective field='accountId' headerText='AccountId' width='120' textAlign='Center' isPrimaryKey={true}></ColumnDirective>
-          <ColumnDirective field='createdDate' headerText='CreatedDate' width='120' textAlign='Center' allowEditing={false}></ColumnDirective>
-          <ColumnDirective field='firstName' headerText='First Name' width='120' textAlign='Center' allowEditing={false}></ColumnDirective>
-          <ColumnDirective field='lastName' headerText='Last Name' textAlign='Center' width='120' allowEditing={false}></ColumnDirective>
-          <ColumnDirective field='address' headerText='Address' textAlign='Center' width='120' allowEditing={false}></ColumnDirective>
-          <ColumnDirective field='gender' headerText='Gender' textAlign='Center' width='120' editType='dropdownedit'></ColumnDirective>
-          <ColumnDirective field='phoneNumber' headerText='PhoneNumber' textAlign='Center' width='120'></ColumnDirective>
-          <ColumnDirective field='dateOfBirth' headerText='Date Of Birth' textAlign='Center' width='120' allowEditing={false}></ColumnDirective>
-          <ColumnDirective field='customerPackages' headerText='CustomerPackages' textAlign='Center' width='120' editType='dropdownedit'></ColumnDirective>
-          <ColumnDirective field='lastModifiedDate' headerText='LastModifiedDate' textAlign='Center' width='120'allowEditing={false}></ColumnDirective>
-        </ColumnsDirective>
-        <Inject
-          services={[
-            Toolbar,
-            Resize,
-            Sort,
-            ContextMenu,
-            Filter,
-            Page,
-            ExcelExport,
-            Edit,
-            PdfExport,
-          ]}
-        />
-      </GridComponent>
+          id="gridcomp"
+          dataSource={patients}
+          allowPaging={true}
+          allowSorting
+          allowExcelExport
+          allowPdfExport
+          editSettings={editing}
+          contextMenuItems={contextMenuItems}
+          toolbar={toolbarOptions}
+          ref={(grid) => (gridInstance = grid)}
+          pageSettings={pageSettings}
+          actionBegin={actionBegin.bind(this)}
+          width="100%"
+        >
+          <ColumnsDirective>
+            <ColumnDirective
+              field="patientID"
+              headerText="PatientID"
+              width="120"
+              textAlign="Center"
+              isPrimaryKey={true}
+            ></ColumnDirective>
+            {/* <ColumnDirective field='createdDate' headerText='CreatedDate' width='120' textAlign='Center' allowEditing={false}></ColumnDirective> */}
+            <ColumnDirective
+              field="firstName"
+              headerText="First Name"
+              width="120"
+              textAlign="Center"
+              allowEditing={false}
+            ></ColumnDirective>
+            <ColumnDirective
+              field="lastName"
+              headerText="Last Name"
+              textAlign="Center"
+              width="120"
+              allowEditing={false}
+            ></ColumnDirective>
+            <ColumnDirective
+              field="dateOfBirth"
+              headerText="Date Of Birth"
+              textAlign="Center"
+              width="120"
+              allowEditing={false}
+            ></ColumnDirective>
+            <ColumnDirective
+              field="gender"
+              headerText="Gender"
+              textAlign="Center"
+              width="120"
+              editType="dropdownedit"
+              template={statusTemplate}
+              edit={{
+                params: {
+                  dataSource: genderStatus,
+                  fields: { value: "gender" },
+                },
+              }}
+            ></ColumnDirective>
+            <ColumnDirective
+              field="phoneNumber"
+              headerText="PhoneNumber"
+              textAlign="Center"
+              width="120"
+            ></ColumnDirective>
+            <ColumnDirective
+              field="address"
+              headerText="Address"
+              textAlign="Center"
+              width="120"
+            ></ColumnDirective>
+            {/* <ColumnDirective field='customerPackages' headerText='CustomerPackages' textAlign='Center' width='120' editType='dropdownedit'></ColumnDirective> */}
+            {/* <ColumnDirective field='lastModifiedDate' headerText='LastModifiedDate' textAlign='Center' width='120'allowEditing={false}></ColumnDirective> */}
+          </ColumnsDirective>
+          <Inject
+            services={[
+              Toolbar,
+              Resize,
+              Sort,
+              ContextMenu,
+              Filter,
+              Page,
+              ExcelExport,
+              Edit,
+              PdfExport,
+            ]}
+          />
+        </GridComponent>
       ) : (
         <p>Loading...</p>
       )}
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   GridComponent,
   ColumnsDirective,
@@ -16,13 +16,22 @@ import {
 } from "@syncfusion/ej2-react-grids";
 import axios from "axios"; // Import Axios
 import { axiosPrivate } from "../api/axiosInstance";
-import { GET_ALL_USERS } from "../api/apiConstants";
+import { GET_ALL_USERS, DATA_OF_USERS } from "../api/apiConstants";
+import { useStateContext } from "../contexts/ContextProvider";
 import { data } from "../data/datasource";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from "react-toastify";
 import { Header } from "../components";
 import { contextMenuItems } from "../data/dummy";
 const Users = () => {
+  const { isClicked, setIsClicked, handleClick, setIsLoggedIn, isLoggedIn } =
+  useStateContext();
+  const [users, setUsers] = useState([]); // State để lưu dữ liệu từ API
+  const [dataLoaded, setDataLoaded] = useState(false);
+  const isMounted = useRef(false); // Add this line
+  const navigate = useNavigate();
   //TEST
-  const toolbarOptions = ['Add', 'Edit', 'Delete', 'Update', 'Cancel'];
+  const toolbarOptions = ['Add', 'Edit', 'Delete', 'Cancel'];
     const editparams = { params: { popupHeight: '300px' } };
     const validationRule = { required: true };
     const customeridRules = { required: true, number: true };
@@ -34,39 +43,217 @@ const Users = () => {
         { text: 'Top', value: 'Top' },
         { text: 'Bottom', value: 'Bottom' }
     ];
-  //call api get data về
+    useEffect(() => {
+      if (!isLoggedIn) {
+        toast.error("User must login first");
+        navigate("/login");
+      }
+    }, []); // Empty dependency array means this effect runs once after the initial render
 
-  const [users, setUsers] = useState([]); // State để lưu dữ liệu từ API
-  const [dataLoaded, setDataLoaded] = useState(false);
-  useEffect(() => {
-    // Create a new CancelToken
-    const source = axios.CancelToken.source();
+    const modifyUserData = (data) => {
+      let userRes = data.map((item, index) => {
+        return {
+          ...item,
+          index: index + 1,
+        };
+      });
+      return userRes;
+    };
   
-    // Make the request, passing the CancelToken
-    axiosPrivate.get(GET_ALL_USERS, { cancelToken: source.token })
-      .then(response => {
-        if (response.status === 200) {
-          console.log("danh sách : ", response.data);
-          // Lưu dữ liệu API vào state
-          setUsers(response.data);
+
+  //call api get data về
+  useEffect(() => {
+    isMounted.current = true; // Set the ref to true when the component mounts
+  
+    const fetchData = async () => {
+      try {
+        const response = await axiosPrivate.get(GET_ALL_USERS);
+        if (response.status === 200 && isMounted.current) {
+          console.log("danh sách users: ", response.data);
+          const userData = modifyUserData(response.data);
+          setUsers(userData);
           setDataLoaded(true);
           console.log("users :", users);
         }
-      })
-      .catch(error => {
-        // If the request was cancelled, log a message to the console
-        if (axios.isCancel(error)) {
-          console.log('Request cancelled:', error.message);
-        } else {
-          console.error("Lỗi khi lấy dữ liệu từ API:", error);
-        }
-      });
+      } catch (error) {
+        console.error("Lỗi khi lấy dữ liệu từ API:", error);
+        toast.error("Cannot get the data please refresh or log back in!");
+      }
+    };
   
-    // Cancel the request if the component unmounts
+    fetchData();
+  
     return () => {
-      source.cancel('Component unmounted');
+      isMounted.current = false; // Set the ref to false when the component unmounts
     };
   }, []);
+  // CUSTOM UserStatus
+  const userStatusIndex = [0, 1].map((userStatus, index) => ({
+    id: index,
+    userStatus,
+  }));
+  const UserStatusTemplate = (field) => {
+    if (field && field.status !== undefined) {
+      const userStatus = userStatusIndex.find((status) => status.id === field.status);
+      const userStatusMap = {
+        0: "Not Active",
+        1: "Active",
+      };
+      console.log("userStatus UI ", userStatus);
+      return (
+        <div>
+          <span>{userStatus ? userStatusMap[userStatus.userStatus] : "Unknown"}</span>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <span>Unknown</span>
+        </div>
+      );
+    }
+  };
+  // CUSTOM UserRole
+  const userRoleIndex = [0, 1, 2].map((userRole, index) => ({
+    id: index,
+    userRole,
+  }));
+  const UserRoleTemplate = (field) => {
+    if (field && field.role !== undefined) {
+      const userRole = userRoleIndex.find((status) => status.id === field.role);
+      const userRoleMap = {
+        0: "Admin",
+        1: "Doctor",
+        2: "Patient",
+      };
+      console.log("userRole UI ", userRole);
+      return (
+        <div>
+          <span>{userRole ? userRoleMap[userRole.userRole] : "Unknown"}</span>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <span>Unknown</span>
+        </div>
+      );
+    }
+  };
+
+  //HANDLE UPDATE
+  const updateUserInState = (oldData, newData) => {
+    setUsers((prevUser) => {
+      return prevUser.map((user) =>
+      user.accountId === oldData.accountId
+          ? { ...oldData, ...newData }
+          : user
+      );
+    });
+  };
+  //userStatus helper
+  const getUserStatusId = (statusName) => {
+    console.log("Status name: ", statusName); // Log the status name
+    console.log("userStatusIndex: ", userStatusIndex); 
+    const status = userStatusIndex.find((status) => status.userStatus === statusName);
+    return status ? status.id : null;
+  };
+  //userRoleStatus helper
+  const getUserRoleStatusId = (statusName) => {
+    console.log("Status name: ", statusName); // Log the status name
+    console.log("userRoleIndex: ", userRoleIndex); 
+    const status = userRoleIndex.find((status) => status.userRole === statusName);
+    return status ? status.id : null;
+  };
+  // CRUD
+  const actionBegin = async (args) => {
+    if (args.data) {
+      console.log("DATA: \n", args.data);
+      console.log("ROW DATA: \n", args.rowData);
+    }
+
+    if (args.requestType === "save") {
+      if (args.action === "add") {
+        // Send a POST request to add a new order
+        await axiosPrivate.post(DATA_OF_USERS, args.data);
+      } else if (args.action === "edit") {
+        // Ensure args.data includes a 'command' field and 'status' is an integer
+        const data = {
+          accountId: args.data.accountId,
+          role: args.data.role,
+          status: args.data.status,
+         
+        };
+        //status
+        const statusId =
+          typeof args.data.status === "number"
+            ? getUserStatusId(args.data.status)
+            : args.data.status;
+        if (statusId !== null) {
+          data.status = statusId;
+        }
+        //role
+        const roleId =
+          typeof args.data.role === "number"
+            ? getUserRoleStatusId(args.data.role)
+            : args.data.role;
+        if (statusId !== null) {
+          data.role = roleId;
+        }
+        console.log("Data: ", data);
+        console.log("User status:", args.data.status);
+        console.log("User role:", args.data.role);
+        // Send a PUT request to update an existing order
+
+        try {
+          // Before sending the PUT request, find the order in the state that you're about to update
+          const userToUpdate = users.find(
+            (user) => user.accountId === args.data.accountId
+          );
+          await axiosPrivate.put(DATA_OF_USERS, data);
+          updateUserInState(userToUpdate, data);
+          toast.success("Update User successfully!");
+        } catch (error) {
+          if (error.response) {
+            console.error(error.response.data); // Log the server's error message
+            toast.error("Update User failed!");
+          } else {
+            console.error(error.message); // Log the error message
+            toast.error("Update User failed!");
+          }
+        }
+      }
+      args.cancel = true; // Cancel the grid update because we've already updated the data source
+    } else if (args.requestType === "delete") {
+      console.log(args.data[0].accountId);
+      const data = {
+        id: args.data[0].accountId,
+      };
+      console.log("DELETE Order id: ", data);
+      // Send a DELETE request to delete an order
+      try {
+        await axiosPrivate.delete(
+          `${DATA_OF_USERS}/${args.data[0].accountId}`
+        );
+        setUsers((prevOrders) =>
+          prevOrders.filter(
+            (user) => user.accountId !== args.data[0].accountId
+          )
+        );
+        toast.success("Update User successfully!");
+      } catch (error) {
+        if (error.response) {
+          console.error(error.response.data); // Log the server's error message
+          toast.error("Update User failed!");
+        } else {
+          console.error(error.message); // Log the error message
+          toast.error("Update User failed!");
+        }
+      }
+      args.cancel = true; // Cancel the grid update because we've already updated the data source
+    }
+  };
+
 
   const editing = { allowDeleting: true, allowEditing: true, allowAdding: true, mode: 'Dialog' };
   return (
@@ -86,14 +273,17 @@ const Users = () => {
         editSettings={editing}
         contextMenuItems={contextMenuItems}
         toolbar={toolbarOptions}
+        ref={(grid) => (gridInstance = grid)}
         pageSettings={pageSettings}
+        actionBegin={actionBegin.bind(this)}
         width="100%"
       >
         <ColumnsDirective>
-          <ColumnDirective field='email' headerText='Email' width='120' textAlign='Center' isPrimaryKey={true} allowEditing={false}></ColumnDirective>
+          <ColumnDirective field='accountId' headerText='AccountId' width='120' textAlign='Center' isPrimaryKey={true} allowEditing={false}></ColumnDirective>
+          <ColumnDirective field='email' headerText='Email' width='120' textAlign='Center' allowEditing={false}></ColumnDirective>
           <ColumnDirective field='password' headerText='Password' textAlign='Center' width='120' allowEditing={false}></ColumnDirective>
-          <ColumnDirective field='role' headerText='Role' textAlign='Center' width='120' editType='dropdownedit'></ColumnDirective>
-          <ColumnDirective field='status' headerText='Status' textAlign='Center' width='120' editType='dropdownedit'></ColumnDirective>
+          <ColumnDirective field='role' headerText='Role' template={UserRoleTemplate}  textAlign='Center' width='120' editType='numericedit'></ColumnDirective>
+          <ColumnDirective field='status' headerText='Status' template={UserStatusTemplate} textAlign='Center' width='120' editType='numericedit'></ColumnDirective>
         </ColumnsDirective>
         <Inject
           services={[
